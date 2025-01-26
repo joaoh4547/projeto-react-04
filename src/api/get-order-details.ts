5b9cce32f9bd55f64194201d6afbb21ff22fa961
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-26 13:18:07 -0300

    Capitulo 3 Parte 14

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,30 @@
+import { api } from "@/lib/axios";
+
+interface GetOrderDetailsInput {
+    orderId: string;
+}
+
+interface GetOrderDetailsData {
+    id: string;
+    createdAt: Date;
+    status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+    totalInCents: number;
+    customer: {
+        name: string;
+        email: string;
+        phone: string | null;
+    };
+    orderItems: {
+        id: string;
+        priceInCents: number;
+        quantity: number;
+        product: {
+            name: string;
+        };
+    }[];
+}
+
+export async function getOrderDetails({ orderId }: GetOrderDetailsInput) {
+    const response = await api.get<GetOrderDetailsData>(`/orders/${orderId}`);
+    return response.data;
+}
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,6 +1,7 @@
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale/pt-BR";
 import { ArrowRight, Search, X } from "lucide-react";
+import { useState } from "react";
 
 import { OrderStatus } from "@/components/order-status";
 import { Button } from "@/components/ui/button";
@@ -25,17 +26,22 @@ interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
+    const [isDetailsOpen, setDetailsOpen] = useState(false);
+
     return (
         <TableRow>
             <TableCell>
-                <Dialog>
+                <Dialog open={isDetailsOpen} onOpenChange={setDetailsOpen}>
                     <DialogTrigger asChild>
                         <Button variant="outline" size="xs">
                             <Search className="h-3 w-3" />
                             <span className="sr-only">Detalhes do pedido</span>
                         </Button>
                     </DialogTrigger>
-                    <OrderDetails />
+                    <OrderDetails
+                        orderId={order.orderId}
+                        opened={isDetailsOpen}
+                    />
                 </Dialog>
             </TableCell>
             <TableCell className="font-mono text-xs font-medium">
@@ -52,7 +58,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
             </TableCell>
             <TableCell className="font-medium">{order.customerName}</TableCell>
             <TableCell className="font-medium">
-                {order.total.toLocaleString("pt-BR", {
+                {(order.total / 100).toLocaleString("pt-BR", {
                     style: "currency",
                     currency: "BRL",
                 })}
