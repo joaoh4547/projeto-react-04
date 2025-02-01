a26be7674cf6739102fc40caea2c92ced0d720d9
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-01 12:03:12 -0300

    Capitulo 5 Parte 4

--- src/components/pagination.spec.tsx
+++ src/components/pagination.spec.tsx
@@ -3,8 +3,11 @@ import userEvent from "@testing-library/user-event";
 
 import { Pagination } from "./pagination";
 
+const onPageChangeCallback = vi.fn();
 describe("Pagination", () => {
-    const onPageChangeCallback = vi.fn();
+    beforeEach(() => {
+        onPageChangeCallback.mockClear();
+    });
 
     it("should display the right amount of pages and results", () => {
         const wrapper = render(
@@ -39,5 +42,68 @@ describe("Pagination", () => {
         await user.click(nextPageButton);
 
         expect(onPageChangeCallback).toHaveBeenCalledWith(1);
+    });
+
+    it("should be able navigate to the previous page", async () => {
+        const wrapper = render(
+            <Pagination
+                pageIndex={5}
+                pageCount={200}
+                perPage={10}
+                onPageChange={onPageChangeCallback}
+            />,
+        );
+
+        const nextPageButton = wrapper.getByRole("button", {
+            name: "Página Anterior",
+        });
+
+        const user = userEvent.setup();
+
+        await user.click(nextPageButton);
+
+        expect(onPageChangeCallback).toHaveBeenCalledWith(4);
+    });
+
+    it("should be able navigate to the first page", async () => {
+        const wrapper = render(
+            <Pagination
+                pageIndex={5}
+                pageCount={200}
+                perPage={10}
+                onPageChange={onPageChangeCallback}
+            />,
+        );
+
+        const nextPageButton = wrapper.getByRole("button", {
+            name: "Primeira Página",
+        });
+
+        const user = userEvent.setup();
+
+        await user.click(nextPageButton);
+
+        expect(onPageChangeCallback).toHaveBeenCalledWith(0);
+    });
+
+    it("should be able navigate to the last page", async () => {
+        const wrapper = render(
+            <Pagination
+                pageIndex={0}
+                pageCount={200}
+                perPage={10}
+                onPageChange={onPageChangeCallback}
+            />,
+        );
+
+        const nextPageButton = wrapper.getByRole("button", {
+            name: "Última Página",
+        });
+
+        const user = userEvent.setup();
+
+        await user.click(nextPageButton);
+
+        expect(onPageChangeCallback).toHaveBeenCalledWith(19);
     });
 });
