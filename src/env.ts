badde7790551d9a13803a0f5bf77c408ed26c6d8
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-01 14:18:07 -0300

    Capitulo 5 Parte 8

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
     MODE: z.enum(["test", "production", "development"]).default("development"),
-    VITE_API_URL: z.string().url(),
+    VITE_API_URL: z.string(),
     VITE_ENABLED_API_DELAY: z.coerce.boolean(),
 });
 
