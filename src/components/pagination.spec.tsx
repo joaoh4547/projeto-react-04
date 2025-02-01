e2b9db2b915a4f9fade0f6246e34bdf42a36cc3f
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-01 11:46:02 -0300

    Capitulo 5 Parte 3

---
+++ src/components/pagination.spec.tsx
@@ -0,0 +1,43 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { Pagination } from "./pagination";
+
+describe("Pagination", () => {
+    const onPageChangeCallback = vi.fn();
+
+    it("should display the right amount of pages and results", () => {
+        const wrapper = render(
+            <Pagination
+                pageIndex={0}
+                pageCount={200}
+                perPage={10}
+                onPageChange={() => {}}
+            />,
+        );
+
+        expect(wrapper.getByText("Página 1 de 20")).toBeInTheDocument();
+        expect(wrapper.getByText("Total de 200 item(s)")).toBeInTheDocument();
+    });
+
+    it("should be able navigate to the next page", async () => {
+        const wrapper = render(
+            <Pagination
+                pageIndex={0}
+                pageCount={200}
+                perPage={10}
+                onPageChange={onPageChangeCallback}
+            />,
+        );
+
+        const nextPageButton = wrapper.getByRole("button", {
+            name: "Proxima Página",
+        });
+
+        const user = userEvent.setup();
+
+        await user.click(nextPageButton);
+
+        expect(onPageChangeCallback).toHaveBeenCalledWith(1);
+    });
+});
